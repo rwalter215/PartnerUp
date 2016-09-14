a1db5b7c0cb726d45507229ababc9da5b64e1c7f
Ryan Walter <r.walter215@gmail.com>
2016-09-14 16:40:24 -0500

    small change knex

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,6 @@
 var url = 'postgres://hzhzhysgpursgg:Mkih7oW9Ek6dGdTSmyuVgxw3kr@ec2-54-163-245-32.compute-1.amazonaws.com:5432/dd9bnae9j8734d'
 
-module.exports = {
-
-  development: {
+module.exports = { 
     client: 'postgresql',
     connection: url,
       // host: 'ec2-54-163-245-32.compute-1.amazonaws.com',
@@ -10,9 +8,7 @@ module.exports = {
       // user:     'hzhzhysgpursgg',
       // password: 'Mkih7oW9Ek6dGdTSmyuVgxw3kr',
       // url: url
-    migrations: {
-      tableName: 'migrations'
-    }
-  }
-
+    // migrations: {
+    //   tableName: 'migrations'
+    // }
 }
